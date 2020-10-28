7b93f06e467eda71981497c06dfec7ea66aee100
Nick Hennessy <njhnny@gmail.com>
2020-10-27 17:18:34 -0700

    fix case-sensitivity bugs

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,17 @@ $(document).ready(function(event) {
   $("form#form").submit(function(event) {
     event.preventDefault();
     $("#output").show();
-    let wordInput = $("input#submission").val();
+    let wordResponse = $("input#submission").val();
+    let wordInput = wordResponse.toLowerCase();
+    
+    //wordInput.toLowerCase();
     let wordString = wordInput.split("");
     let reverseArray = wordString.reverse();
     let reverseWord = reverseArray.join("");
     if (reverseWord === wordInput) {
-      result = ("Yes! " + wordInput + " is a palindrome!");
+      result = ("Yes! " + wordResponse + " is a palindrome!");
     } else {
-       result = ("No, " + wordInput + " is not a palindrome.")
+       result = ("No, " + wordResponse + " is not a palindrome.")
     }
     $("p").text(result);
   });
